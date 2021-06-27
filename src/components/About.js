83145c5c6809de348b13ea545d9af0aced68447a
神隐` <llgdong@qq.com>
2021-06-27 18:17:25 +0800

    use cards

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,5 @@
 import React from "react";
-import { Image, Card, Icon} from "semantic-ui-react";
 import "./about.css"
-import {Link} from "react-router-dom";
 export class About extends React.Component {
 	render() {
 		return (
